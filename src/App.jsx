77ae4a975dc4a4c1b5e32c25e91cb760cf6b249a
MdTabrezHossain <tabrezhusn007@gmail.com>
2025-05-17 18:52:36 +0530

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import { Outlet } from 'react-router-dom';
 import Header from './components/UI/Header.jsx';
 import Footer from './components/UI/Footer.jsx';
 
+import "./App.css";
+
 export default function App() {
   return (
     <>
--- src/components/UI/Header.jsx
+++ src/components/UI/Header.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 export default function Header() {
     return (
-        <div>
-            Header
-        </div>
+        <>
+            <header>
+                <nav className="navbar grid">
+                    <div className='navbar_logo_container'>
+                        <NavLink to="/" className={"font-xx-large font-bold text-white link-no-underline"}>
+                            World Atlas
+                        </NavLink>
+                    </div>
+                    <div>
+                        <ul>
+                            <li className='navbar_ul_li width-full'>
+                                <NavLink to="/home" className={"font-bold text-white link-no-underline"}>Home</NavLink>
+                                <NavLink to="/countries" className={"font-bold text-white link-no-underline"}>Countries</NavLink>
+                                <NavLink to="/about" className={"font-bold text-white link-no-underline"}>About</NavLink>
+                                <NavLink to="/contact" className={"font-bold text-white link-no-underline"}>Contact</NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </header >
+        </>
     )
 }
