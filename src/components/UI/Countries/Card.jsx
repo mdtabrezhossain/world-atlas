4774f696b3324c977b240be0fc1dd9ad7b8063c2
MdTabrezHossain <tabrezhusn007@gmail.com>
2025-06-06 17:50:35 +0530

    added the countries page

--- src/components/UI/Countries/Card.jsx
+++ src/components/UI/Countries/Card.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import "./Card.css";
 
-export default function Card() {
+export default function Card({ countryFlag, countryName, countryCapital, countryCurrency }) {
     return (
         <>
             <div className="country-card">
-                <img className="country-card_image" loading="lazy" />
-                <div className="country-card_content">
-                    <p>Country</p>
-                    <p>Capital</p>
-                    <p>Currency</p>
+                <div className="country-card_image-container">
+                    <img className="country-card_image" loading="lazy" src={countryFlag} />
                 </div>
-            </div>
+                <div className="country-card_content">
+                    <p>Name: <strong>{countryName}</strong></p >
+                    <p>Capital: <strong>{countryCapital}</strong></p >
+                    <p>Currency: <strong>{countryCurrency}</strong></p >
+                </div>
+            </div >
+
         </>
     )
 }
+
--- src/pages/Countries/Countries.jsx
+++ src/pages/Countries/Countries.jsx
@@ -1,30 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
 import { getAllCountriesData } from "../../services/CountriesApi";
 import "./Countries.css";
-import Card from "../../components/UI/Countries/Card";
+import Card from "../../components/UI/Countries/Card.jsx";
 
-async function handleGetAllCountriesData() {
-    const allCountries = await getAllCountriesData();
-    allCountries.forEach(country => {
-        console.log(country.flags.svg);
-        console.log(country.name.common);
-        console.log(country.capital[0]);
-        if (Object.keys(country.currencies).length !== 0) {
-            let currencies = Object.entries(country.currencies);
-            console.log(currencies[0][1].name);
-        }
-    });
-}
+
 
 export default function Countries() {
+    const [allCountries, setAllCountries] = useState([]);
+
     useEffect(() => {
         handleGetAllCountriesData();
     }, []);
 
+    async function handleGetAllCountriesData() {
+        setAllCountries(await getAllCountriesData());
+        // allCountries.forEach(country => {
+        //     console.log(country.flags.svg);
+        //     console.log(country.name.common);
+        //     console.log(country.capital[0]);
+        //     if (Object.keys(country.currencies).length !== 0) {
+        //         let currencies = Object.entries(country.currencies);
+        //         console.log(currencies[0][1].name);
+        //     }
+        // });
+    }
+
     return (
         <>
-            <div className="cards-container">
+            <div className="countries-section">
+                <p className="countries-section_title">
+                    Meet The Countries
+                </p>
+                <div className="country-cards-container">
+                    {
+                        allCountries.map((country, index) => (
+                            <Card
+                                key={index}
+                                countryFlag={country.flags.svg}
+                                countryName={country.name.common}
+                                countryCapital={country.capital[0]}
+                                countryCurrency={
+                                    (Object.keys(country.currencies).length != 0) ? Object.entries(country.currencies)[0][1].name : ""
+                                }
+                            />
+                        ))
+                    }
+                </div>
             </div>
         </>
     )
